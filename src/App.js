581281b87d7b9515e85b617b5259da04f082c45e
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-07-19 20:22:32 +0200

    Added new components for router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 
 const items = [
   {
@@ -37,48 +39,23 @@ const options = [
 
 function App() {
   const [selected, setSelected] = useState(options[0])
-  //const [showDropdown, setShowDropdown] = useState(true)
 
-  const showAccordion = () => {
-    if (window.location.pathname === '/') {
-      return <Accordion items={items}/>
-    }
-  }
-
-  const showList = () => {
-    if (window.location.pathname === '/list') {
-      return <Search />
-    }
-  }
-
-  const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-      return <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
-    
-    }
-  }
-    
-  const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-      return   <Translate />
-    }
-  }
   return (
     <div className="App">
       <h1>Widgets</h1>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
-      {/* <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle DropDown</button> */}
-     
-      {/* <Search /> */}
-      {/* {
-        showDropdown ?
-          <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
-          : null
-      } */}
-      
+      <Header />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
+        <Dropdown label='Select color' options={options} selected={selected} onSelectedChange={setSelected} />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     
     </div>
   );
---
+++ src/components/Link.js
@@ -0,0 +1,16 @@
+
+const Link = ({ className, href, children }) => {
+  const handleClick = (e) => {
+    e.preventDefault()
+    window.history.pushState({}, '', href)
+
+    const navEvent = new PopStateEvent('popstate')
+    window.dispatchEvent(navEvent)
+  }
+
+  return (
+    <a className={className} href={href} onClick={handleClick}>{children}</a>
+  )
+}
+
+export default Link

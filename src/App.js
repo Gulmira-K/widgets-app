e96be87ae08bdc9330d62ea69502f7bec2239e5f
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-07-19 12:17:03 +0200

    Modified traslate widget, started the implementation of router (without React Router)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import {useState} from 'react'
-//import Accordion from './components/Accordion'
-// import Search from './components/Search'
-// import Dropdown from './components/Dropdown'
+import Accordion from './components/Accordion'
+import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
 
-// const items = [
-//   {
-//     title: 'What is React?',
-//     content: 'React is a front end javascript framework'
-//   },
-//   {
-//     title: 'Why use React?',
-//     content: 'React is a favorite JS library among engineers'
-//   },
-//   {
-//     title: 'How do you use React?',
-//     content: 'You use React by creating components'
-//   },
-// ]
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  },
+]
 
 const options = [
   {
@@ -34,15 +34,44 @@ const options = [
   }
 ]
 
-function App() {
-  // const [selected, setSelected] = useState(options[0])
-  // const [showDropdown, setShowDropdown] = useState(true)
 
+function App() {
+  const [selected, setSelected] = useState(options[0])
+  //const [showDropdown, setShowDropdown] = useState(true)
+
+  const showAccordion = () => {
+    if (window.location.pathname === '/') {
+      return <Accordion items={items}/>
+    }
+  }
+
+  const showList = () => {
+    if (window.location.pathname === '/list') {
+      return <Search />
+    }
+  }
+
+  const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+      return <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
+    
+    }
+  }
+    
+  const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+      return   <Translate />
+    }
+  }
   return (
     <div className="App">
       <h1>Widgets</h1>
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
       {/* <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle DropDown</button> */}
-      {/* <Accordion items={items}/> */}
+     
       {/* <Search /> */}
       {/* {
         showDropdown ?
@@ -50,7 +79,7 @@ function App() {
           : null
       } */}
       
-      <Translate />
+    
     </div>
   );
 }
